9a47edd14303b8183854c5173290d1dba6e53d3b
Thomas Peek <peekthomas369@icloud.com>
2023-01-26 16:05:49 -0700

    update

--- app.js
+++ app.js
@@ -2,21 +2,42 @@ const heroes = [
     {
         name: 'Thomas',
         type: 'StormTrooper',
-        damage: 100,
+        damage: 10,
         health: 110,
         level: 1,
         gold: 1,
+        isActive: true,
         img: 'https://images.unsplash.com/photo-1472457897821-70d3819a0e24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1738&q=80"'
     },
     {
         name: 'Taylor',
         type: 'Banana',
-        damage: 125,
+        damage: 12,
         health: 115,
         level: 1,
         gold: 1,
+        isActive: true,
         img:"https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1839&q=80"
-
+    },
+    {
+        name: 'OddBall',
+        type: 'bathtub',
+        damage: 1,
+        health: 100,
+        level: 1,
+        gold: 1,
+        isActive: false,
+        img:"https://images.unsplash.com/photo-1626792133251-db513424bc2a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"
+    },
+    {
+        name: 'Bjorg',
+        type: 'cyborg',
+        damage: 125,
+        health: 500,
+        level: 1,
+        gold: 1,
+        isActive: false,
+        img:"https://plus.unsplash.com/premium_photo-1671229062407-979e731e7bd2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
     }
 ]
 
@@ -31,7 +52,12 @@ const boss = {
 function heroesAttackBoss(){
     let combinedDamage = 0
     heroes.forEach(hero => {
-       combinedDamage += hero.damage 
+        if (hero.isActive) {
+            combinedDamage += hero.damage 
+            if (hero.health == 0) {
+             combinedDamage = 0
+            }
+        }
     });
     // console.log(combinedDamage)
     
@@ -41,7 +67,13 @@ if(boss.health <= 0){
     boss.maxHealth = boss.maxHealth + 100
     boss.damage = boss.damage + 5
     boss.level = boss.level + 1
+    heroes.forEach(hero => {
+        hero.gold = hero.gold + 50
+        hero.level = hero.level + 1
+    });
    }
+   updateHeros()
+   updateBoss()
     console.log(boss.health)
 }
 
@@ -51,7 +83,7 @@ function bossAttacksHeroes(){
     heroes.forEach(hero => {
         hero.health = hero.health - boss.damage
         if(hero.health <= 0){
-            hero.health == 0
+            hero.health = 0
 // window.alert('You Are Dead and You lose')
         }
     });
@@ -77,8 +109,61 @@ function updateHeros(){
     })
 }
 
+function updateBoss(){
+    let bossElem = document.getElementById('boss')
+    bossElem.innerHTML = `
+                        <h1>THE BOSS!!!</h1>
+                        <h2>Health: ${boss.health}/${boss.maxHealth}</h2>
+                        <h3>Damage:${boss.damage}</h3>
+    
+    `
+}
 
 
+function buyPotion(){
+    heroes.forEach(hero => {
+    hero.gold = hero.gold - 20
+    hero.health = hero.health + 50
+    });
+ updateHeros()
+    console.log("this works")
+}
+
+function buyOddBall(){
+    console.log('you bought oddball');
+heroes.forEach(hero =>{
+    hero.gold = hero.gold - 50
+})
+heroes[2].isActive = true
+let oddballElem = document.getElementById('hidden')
+oddballElem.innerHTML = `
+            <div class="col-3">
+                <div class="card" id="OddBall">
+                    <h1></h1>
+                    <h2></h2>
+                    <h2></h2>
+                    <h2></h2>
+                    <img src='' alt="">
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card" id="Bjorg">
+                    <h1></h1>
+                    <h2></h2>
+                    <h2></h2>
+                    <h2></h2>
+                    <img src="" alt="">
+                </div>
+            </div>
+`
+}
+
+function buyBjorg(){
+    console.log('you bought bjorg');
+}
+
+
+updateBoss()
 setInterval(bossAttacksHeroes, 10000)
 console.log(boss);
 updateHeros()
